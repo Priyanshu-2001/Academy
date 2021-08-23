d5c3a2d0d6bf3e50016b08af6395513232ccd36e
Priyanshu-2001 <priyanshuhupta123123@gmail.com>
2021-08-23 16:16:52 +0530

    app icon added

--- app/src/main/java/com/sports/oscaracademy/data/DashBoardData.java
+++ app/src/main/java/com/sports/oscaracademy/data/DashBoardData.java
@@ -1,13 +1,13 @@
 package com.sports.oscaracademy.data;
 
-import android.app.Application;
 import android.widget.ImageView;
 
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.databinding.BindingAdapter;
-import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
 import com.bumptech.glide.Glide;
+import com.sports.oscaracademy.R;
 
 public class DashBoardData extends ViewModel {
     public String ImageUrl , fieldname;
@@ -30,6 +30,6 @@ public class DashBoardData extends ViewModel {
         Glide.with(imageView)
                 .load(URL)
                 .into(imageView)
-        ;
+                .onLoadFailed(AppCompatResources.getDrawable(imageView.getContext(), R.drawable.app_icon_main));
     }
 }
